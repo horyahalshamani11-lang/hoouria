7c0e7d0b4f5c511e02ecf6a6cdf3719d70ac9c92
horyahalshamani11-lang <horyahalshamani11@gmail.com>
2025-11-30 01:30:16 +0300

    Update untitled29_py (3).py

--- untitled29_py (3).py
+++ untitled29_py (3).py
@@ -55,7 +55,7 @@ if st.button("Predict"):
   event2=event1.transform([event])
   transform=itertools.chain([nei2,day2,hour,temp,weather2,weekend2,event2,acc])
   transform2=list(transform)
-  transform3=np.array(transform2).reshape(1,-1)
+  transform3=np.array(transform2).reshape(-1,1)
   pred=model.predict(transform3)
   st.success("The traffic level is{}".format(pred))
 
